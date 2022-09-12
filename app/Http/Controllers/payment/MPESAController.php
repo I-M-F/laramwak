182a20de97f7802d9f514259e27edbc8ba0182e0
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2022-09-12 11:28:25 +0300

    mwak test regan-platform update payments 4

--- app/Http/Controllers/payment/MPESAController.php
+++ app/Http/Controllers/payment/MPESAController.php
@@ -109,9 +109,90 @@ class MPESAController extends Controller
         return $lipa_na_mpesa_password;
     }
 
+    public function mwakSTKPush($phone){
+                # access token
+        $consumerKey = 'o1QYI6OQjLTNrvJbFYreRjacWHEh9fxe'; //Fill with your app Consumer Key
+        $consumerSecret = 'xT2hd4Kirj5H0AY3'; // Fill with your app Secret
+
+        # define the variales
+        # provide the following details, this part is found on your test credentials on the developer account
+        $BusinessShortCode = '174379';
+        $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
+        
+        $paymentDB = DB::table('payments')
+            ->where('phone', '=', $phone)
+            ->first();
+     
+        
+        
+        $PartyA = preg_replace("/^0/", "254", $phone); // This is your phone number, 
+        $AccountReference = 'MWAK';
+        $TransactionDesc = 'MWAK Payment';
+        $Amount = $paymentDB->amount;
+        
+        # Get the timestamp, format YYYYmmddhms -> 20181004151020
+        $Timestamp = date('YmdHis');    
+        
+        # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+        $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
+
+        # header for access token
+        $headers = ['Content-Type:application/json; charset=utf8'];
+
+            # M-PESA endpoint urls
+        $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+        $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+        # callback url
+        $CallBackURL = 'https://morning-basin-87523.herokuapp.com/callback_url.php';  
+
+        $curl = curl_init($access_token_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_HEADER, FALSE);
+        curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+        $result = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $result = json_decode($result);
+        $access_token = $result->access_token;  
+        curl_close($curl);
+
+        # header for stk push
+        $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
+
+        # initiating the transaction
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $initiate_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+        $curl_post_data = array(
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => $BusinessShortCode,
+            'Password' => $Password,
+            'Timestamp' => $Timestamp,
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => $Amount,
+            'PartyA' => $PartyA,
+            'PartyB' => $BusinessShortCode,
+            'PhoneNumber' => $PartyA,
+            'CallBackURL' => $CallBackURL,
+            'AccountReference' => $AccountReference,
+            'TransactionDesc' => $TransactionDesc
+        );
+
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        print_r($curl_response);
+
+        echo $curl_response;
+    }
+
     public function mpesaSTKPush($phone){
 
-            $simu =preg_replace("/^0/", "254", $phone);
+            $simu = preg_replace("/^0/", "254", $phone);
         
             $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
             $lipa_time = Carbon::rawParse('now')->format('YmdHms');
