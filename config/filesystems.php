45cac6c114033b2cc1c52d1c397d5a656564c3df
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2022-09-22 19:49:13 +0300

    mwak make it better reg form dd config

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,34 +48,34 @@ return [
 
        
 
-        'dropbox' => [
-            'driver' => 'dropbox',
-            'key' => env('DROPBOX_APP_KEY'),
-            'secret' => env('DROPBOX_APP_SECRET'),
-            'authorization_token' => env('DROPBOX_AUTH_TOKEN'),
-        ],
+        // 'dropbox' => [
+        //     'driver' => 'dropbox',
+        //     'key' => env('DROPBOX_APP_KEY'),
+        //     'secret' => env('DROPBOX_APP_SECRET'),
+        //     'authorization_token' => env('DROPBOX_AUTH_TOKEN'),
+        // ],
 
    
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-        ],
+        // 's3' => [
+        //     'driver' => 's3',
+        //     'key' => env('AWS_ACCESS_KEY_ID'),
+        //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        //     'region' => env('AWS_DEFAULT_REGION'),
+        //     'bucket' => env('AWS_BUCKET'),
+        //     'url' => env('AWS_URL'),
+        //     'endpoint' => env('AWS_ENDPOINT'),
+        //     'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        //     'throw' => false,
+        // ],
 
-        'spaces' => [
-            'driver' => 's3',
-            'key' => env('DO_SPACES_KEY'),
-            'secret' => env('DO_SPACES_SECRET'),
-            'endpoint' => env('DO_SPACES_ENDPOINT'),
-            'region' => env('DO_SPACES_REGION'),
-            'bucket' => env('DO_SPACES_BUCKET'),
-         ],
+        // 'spaces' => [
+        //     'driver' => 's3',
+        //     'key' => env('DO_SPACES_KEY'),
+        //     'secret' => env('DO_SPACES_SECRET'),
+        //     'endpoint' => env('DO_SPACES_ENDPOINT'),
+        //     'region' => env('DO_SPACES_REGION'),
+        //     'bucket' => env('DO_SPACES_BUCKET'),
+        //  ],
 
     ],
 
