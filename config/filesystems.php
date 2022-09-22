42ac2b8db7f85f779339a58e1df0337b9ebba248
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2022-09-22 18:38:15 +0300

    mwak make it better reg form dd config x

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,13 +34,13 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('appx'),
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('appx/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
