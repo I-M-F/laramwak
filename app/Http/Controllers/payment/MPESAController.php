767ef136743cd06099428daad363d74623e10bb3
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2022-11-16 15:27:12 +0300

    make it better - pay notification first to get

--- app/Http/Controllers/payment/MPESAController.php
+++ app/Http/Controllers/payment/MPESAController.php
@@ -293,7 +293,7 @@ class MPESAController extends Controller
         //dd($phone);
 
         $tuma = DB::table('payments')->where('id', $id)->first();
-        $member = DB::table('member_registartions')->where('phone', $tuma->phone)->first();
+        $member = DB::table('member_registartions')->where('phone', $tuma->phone)->get();
         //dd($edit);
         return view('backend.user.tuma-sms', compact('tuma', 'member'));
     }
