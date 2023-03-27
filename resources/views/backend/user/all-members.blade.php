4a9e26ea7335971d8f1148a34eb317ab1ae3e798
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-03-27 12:05:17 +0300

    member reg display all  details foreach viewM 2

--- resources/views/backend/user/all-members.blade.php
+++ resources/views/backend/user/all-members.blade.php
@@ -45,17 +45,17 @@
                     <td>{{ $row['member']->phone }}</td>
                     <td>{{ $row['member']->id_number }}</td>
                     <td>{{ $row['county']->name }}</td>
-                    <td>{{ $subCountyDB->name }}</td>
-                    <td>{{ $row->phone }}</td>
-                    <td>{{ $row->spouse_name.' '.$row->spouse_maiden_name}}</td>
-                    @if($row->role=='Unverified')
-                    <td style="background-color:green ">{{ $row->service_number }}</td>
+                    <td>{{ $row['sub_county']->name }}</td>
+                    <td>{{ $row['member']->phone }}</td>
+                    <td>{{ $row['member']->spouse_name.' '.$row['member']->spouse_maiden_name}}</td>
+                    @if($row['member']->role=='Unverified')
+                    <td style="background-color:green ">{{ $row['member']->service_number }}</td>
                     @elseif($row->role=='Rejected')
-                    <td style="background-color:red ">{{ $row->service_number }}</td>
+                    <td style="background-color:red ">{{ $row['member']->service_number }}</td>
                     @else
-                    <td>{{ $row->service_number }}</td>
+                    <td>{{ $row['member']->service_number }}</td>
                     @endif
-                    <td>{{ $row->class }}</td>
+                    <td>{{ $row['member']->class }}</td>
                     <td><img src=" {{ asset('/storage/'. substr($row->id_card,6)) }}" alt="" title="{{ substr($row->id_card,6) }}" width='50' height='50' class="img img-responsive"> </td>
 
                     <td>
