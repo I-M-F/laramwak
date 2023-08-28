516ddb4b5897e8da16f39b31afea1aa88c621eb6
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-08-28 09:52:55 +0300

    test login commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form method="POST" action="{{ route('loginx') }}">
+      <form method="POST" action="{{ route('login') }}">
                         @csrf
         <div class="input-group mb-3">
           <input type="email" name="email" class="form-control" placeholder="Email">
