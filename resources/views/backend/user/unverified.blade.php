671571e61a4abf02413c21d68248b2aafe62a2c8
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-08-29 11:55:18 +0300

    test update & Pay  commit

--- resources/views/backend/user/unverified.blade.php
+++ resources/views/backend/user/unverified.blade.php
@@ -8,10 +8,14 @@
                 <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
                     <i class="fa fa-frown-o" aria-hidden="true" style="font-size:48px;color:red"></i>
                     @if ($member_dets->role == 'Rejected')
+                    <i class="fa fa-frown-o" aria-hidden="true" style="font-size:48px;color:red"></i>
+
                     <h2 class="m-0">Rejected</h2>
                     <h6>User <span class="text-info bg-dark">{{$member_dets->name}}</span> Rejected - mwak.co.ke</h6>
                     <p>If Rejected, please contact Admin <br><span class="text-info"><a href="tel:+254718111186">+254 718111186</a></span> <br> or <br> email <span class="text-info">info@mwak.ke</span> <br> Sorry for the inconvenience.</p>
                     @elseif ($member_dets->role == 'Unverified')
+                    <i class="fa fa-frown-o" aria-hidden="true" style="font-size:48px;color:red"></i>
+
                     <h2 class="m-0">Unverified</h2>
                     <h6>User <span class="text-info bg-dark">{{$member_dets->name}}</span> not verified - mwak.co.ke</h6>
                     <p>If not verified in the next 48 hours, please contact Admin <br><span class="text-info"><a href="tel:+254718111186">+254 718111186</a></span> <br> or <br> email <span class="text-info">info@mwak.ke</span> <br> Sorry for the inconvenience.</p>
