83bd3376b950f1c148a0323674cbe4d62c36689c
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-08-29 09:43:18 +0300

    test update & Pay  commit

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
     <img src="{{asset('backend/dist/img/logo.jpg')}}" alt="MWAK Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">MWAK Portal</span>
+    <span class="brand-text font-weight-light">MWAK Portal </span>
   </a>
 
   <!-- Sidebar -->
