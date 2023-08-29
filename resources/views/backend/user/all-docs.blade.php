ed0b6b385805349bc2e467ad43db5211c03c0466
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-08-29 12:08:26 +0300

    test update & Pay  commit

--- resources/views/backend/user/all-docs.blade.php
+++ resources/views/backend/user/all-docs.blade.php
@@ -1,69 +1,103 @@
 @extends('backend.layouts.app')
 @section('content')
 
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Welcome to {{auth()->user()->role}} - Dashboard</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Member Dashboard </li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
 
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Info boxes -->
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box">
+            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
+            <div class="info-box-content">
+              <span class="info-box-text">Approved Members</span>
+              <span class="info-box-number">
+                <!-- {{$approved_members}} -->
 
-
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">MWAK Documents List</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                    <th>Doc No. </th>
-                    <th>Description</th>
-                    <th>Date</th>   
-                    <th>Action</th>              
-                  </tr>
-                  </thead>
-                  <tbody>
-           
-                  @foreach($all as $key=>$row)
-                    <tr>
-                        <td>{{ $key+1 }}</td>
-                        <td>{{ $row->description }}</td>
-                        <td>{{ $row->date }}</td>
-                                            
- </td>
-                        <td>
-                            <a href="{{ URL::to('/view-docs/'.$row->id) }}" class="btn btn-sm btn-info">View</a>
-                            <!-- <a href="{{ URL::to('/delete-user/'.$row->id) }}" class="btn btn-sm btn-danger">Delete</a> -->
-                        </td>
-
-                    </tr>
-                    @endforeach
-
-
-                  </tbody>
-                  <tfoot>
-                  <tr>
-                  <th>Doc No. </th>
-                    <th>Description</th>
-                    <th>Date</th> 
-                    <th>Action</th>   
-                  </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
+              </span>
             </div>
-            <!-- /.card -->
+            <!-- /.info-box-content -->
           </div>
-          <!-- /.col -->
+          <!-- /.info-box -->
         </div>
-        <!-- /.row -->
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">New Members</span>
+              <!-- <span class="info-box-number">{{$new_member}}</span> -->
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix hidden-md-up"></div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Approved Payments</span>
+              <!-- <span class="info-box-number">{{$approved_payments}}</span> -->
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="info-box mb-3">
+            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Pending Payments</span>
+              <!-- <span class="info-box-number">{{$pending_payments}}</span> -->
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
       </div>
-      <!-- /.container-fluid -->
-    </section>
-    </div>
+      <!-- /.row -->
+
+
+      <!-- /.row -->
+
+      <!-- Main row -->
+
+      <!-- /.row -->
+    </div><!--/. container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
 @endsection
