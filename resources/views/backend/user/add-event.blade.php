bcf546f76aa078457411e78233f552058ab117dd
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-14 11:41:48 +0300

    add event commit

--- resources/views/backend/user/add-event.blade.php
+++ resources/views/backend/user/add-event.blade.php
@@ -5,11 +5,8 @@
     <section class="content">
         <div class="row">
             <div class="col-lg-1">
-
             </div>
             <div class="col-lg-10">
-
-
                 <!-- card start -->
                 <div class="card">
                     @if($errors->any())
@@ -20,18 +17,31 @@
                     </div>
                     <!-- Start Card Body -->
                     <div class="card-body">
-                        <form role="form" action="{{URL::to('upload-docs')}}" enctype="multipart/form-data" method="POST">
+                        <form role="form" action="{{ URL::to('upload-docs') }}" enctype="multipart/form-data" method="POST">
                             @csrf
-                 
 
                             <div class="form-group row">
-                                <label for="name" class="col-sm-2 col-form-label"> Title</label>
+                                <label for="event_title" class="col-sm-2 col-form-label"> Title</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="event_title" placeholder="Enter Event Title" required>
+                                    <input type="text" class="form-control" name="event_title" id="event_title" placeholder="Enter Event Title" required>
                                 </div>
                             </div>
 
+                            <!-- Add Date Selection for Event Start -->
+                            <div class="form-group row">
+                                <label for="event_start" class="col-sm-2 col-form-label">Event Start</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control datepicker" name="event_start" id="event_start" placeholder="Select Event Start Date" required>
+                                </div>
+                            </div>
 
+                            <!-- Add Date Selection for Event End -->
+                            <div class="form-group row">
+                                <label for="event_end" class="col-sm-2 col-form-label">Event End</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control datepicker" name="event_end" id="event_end" placeholder="Select Event End Date" required>
+                                </div>
+                            </div>
 
                             <!-- End card body -->
                             <div class="card-footer">
@@ -43,10 +53,18 @@
             </div>
             <!-- Card End  -->
             <div class="col-lg-1">
-
             </div>
         </div>
     </section>
 </div>
+
+<script>
+    // Initialize Bootstrap Datepicker
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd', // Define your preferred date format
+        autoclose: true,
+        todayHighlight: true,
+    });
+</script>
 
 @endsection
