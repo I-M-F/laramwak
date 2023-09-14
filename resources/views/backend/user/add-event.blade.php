ac7865257c4cfe8ae89a2a9928661dc16f3209cc
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-14 12:11:43 +0300

    add event commit

--- resources/views/backend/user/add-event.blade.php
+++ resources/views/backend/user/add-event.blade.php
@@ -31,16 +31,16 @@
                             <div class="form-group row">
                                 <label for="event_start" class="col-sm-2 col-form-label">Event Start</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control datepicker" name="event_start" id="event_start" placeholder="Select Event Start Date" required>
+                                    <input type="text" class="form-control datetimepicker" name="event_start" id="event_start" placeholder="Select Event Start Date" required>
                                 </div>
                             </div>
-            
+
 
                             <!-- Add Date Selection for Event End -->
                             <div class="form-group row">
                                 <label for="event_end" class="col-sm-2 col-form-label">Event End</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control datepicker" name="event_end" id="event_end" placeholder="Select Event End Date" required>
+                                    <input type="text" class="form-control datetimepicker" name="event_end" id="event_end" placeholder="Select Event End Date" required>
                                 </div>
                             </div>
 
