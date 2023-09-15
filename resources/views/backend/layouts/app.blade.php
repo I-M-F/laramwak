21acc0d1c9080d993ddcbf0ed9e48ab37cb3e1f9
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-15 18:01:17 +0300

    photo displaycommit

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -31,10 +31,22 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
 
 
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
 
-  <!-- Toaster Notification -->
-  <link rel="stylesheet" href="{{asset('toaster/toastr.min.css')}}">
+<body>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
+</body>
+
+</html>
+
+
+<!-- Toaster Notification -->
+<link rel="stylesheet" href="{{asset('toaster/toastr.min.css')}}">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
