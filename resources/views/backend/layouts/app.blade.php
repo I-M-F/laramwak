b66d44616a9bf8fa4f6d69bab19c016966ba812a
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-15 18:03:38 +0300

    photo displaycommit

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -27,26 +27,14 @@
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 
   <!-- Add these links datepicker -->
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
 
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
+  
 
-<body>
-
-  <div class="container">
-    @yield('content')
-  </div>
-
-</body>
-
-</html>
-
-
-<!-- Toaster Notification -->
-<link rel="stylesheet" href="{{asset('toaster/toastr.min.css')}}">
+  <!-- Toaster Notification -->
+  <link rel="stylesheet" href="{{asset('toaster/toastr.min.css')}}">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
