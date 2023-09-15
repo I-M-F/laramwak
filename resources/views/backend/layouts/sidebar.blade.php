b3d26b6d923800157e3f86519609aca93a445e5c
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-15 19:17:02 +0300

    app photo commit

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -13,7 +13,7 @@
         <img src="{{ asset('/storage'.substr(auth()->user()->photo,6)) }}" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">{{auth()->user()->name}} </a>
+        <a href="#" class="d-block">{{auth()->user()->photo}} </a>
       </div>
     </div>
 
