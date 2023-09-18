4727bb7e5cfbb01a726258b547ac1860a7778d8f
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-18 15:55:35 +0300

    update  member dets

--- resources/views/backend/user/all-docs.blade.php
+++ resources/views/backend/user/all-docs.blade.php
@@ -13,7 +13,7 @@
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Member Dashboard {{ $member_dets }} - {{ $totalMembers }} - {{ $totalEvents }} - {{ $paymentDB }} - {{ $pendingPayments }}</li>
+            <li class="breadcrumb-item active">Member Dashboard {{ $totalMembers }} </li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
