ba374c0f519696bd119c090126ee13f30352a581
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-26 09:58:26 +0300

    commit edit pagination script

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -277,9 +277,9 @@
 
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <!-- <script>
+  <script>
     var jQueryForDatePicker = jQuery.noConflict(true);
-  </script> -->
+  </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
