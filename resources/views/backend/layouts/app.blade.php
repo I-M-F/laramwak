8b12f6b848fa4215a3de21b278683eb4adfb218e
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-26 12:08:32 +0300

    commit edit pagination script

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -102,7 +102,7 @@
   <!-- AdminLTE for demo purposes -->
   <!-- <script src="{{asset('backend/dist/js/demo.js')}}"></script> -->
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <script src="{{asset('backend/dist/js/pages/dashboard2.js')}}"></script>
+  <!-- <script src="{{asset('backend/dist/js/pages/dashboard2.js')}}"></script> -->
 
   <!-- DataTables  & Plugins -->
   <script src="{{asset('backend/plugins/datatables/jquery.dataTables.min.js')}}"></script>
