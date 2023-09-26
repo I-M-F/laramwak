e216baf1563e1042b7dabe52816cb754c1dd445d
Matimu Ignatius <87519759+I-M-F@users.noreply.github.com>
2023-09-26 12:09:47 +0300

    commit edit pagination script

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -278,9 +278,9 @@
 
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script>
+  <!-- <script>
     var jQueryForPagination = jQuery.noConflict(true);
-  </script>
+  </script> -->
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
